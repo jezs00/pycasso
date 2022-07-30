2be8970620e6a2a2d2fef81ebb9f5c4a4ff49836
Jez <contact@jessfarber.com>
2022-07-30 19:05:25 +1000

    altered thumbnail method to center image based on epd and provide correct size to buffer

--- screentest.py
+++ screentest.py
@@ -12,9 +12,11 @@ contentDirectory = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'te
 #    sys.path.append(libdir)
 
 import logging
+# TODO: refactor with omni-epd https://github.com/robweber/omni-epd
 from waveshare_epd import epd7in5_V2
 import time
-from PIL import Image, ImageDraw, ImageFont
+# TODO: remove ImageShow testing once ready for SIT
+from PIL import Image, ImageDraw, ImageFont, ImageShow
 import traceback
 
 logging.basicConfig(level=logging.DEBUG)
@@ -42,19 +44,27 @@ try:
     logging.info(epdResolution)
     imageBase.thumbnail(epdResolution)
 
-    # Make sure image is correct size after thumbnail set
-    #imageBase.crop((0,0,epd.width, epd.height))
+    # Make sure image is correct size and centered after thumbnail set
+    widthDiff = (epd.width - imageBase.width)/2
+    heightDiff = (epd.height - imageBase.height)/2
+    leftPixel = 0 - widthDiff
+    topPixel = 0 - heightDiff
+    rightPixel = imageBase.width + widthDiff
+    bottomPixel = imageBase.height + heightDiff
+    imageCrop = (leftPixel, topPixel, rightPixel, bottomPixel)
+    imageBase = imageBase.crop(imageCrop)
     logging.info(imageBase.width)
     logging.info(imageBase.height)
     draw = ImageDraw.Draw(imageBase)
 
     # TODO: do we need the rectangle? place it elsewhere maybe
-    draw.rectangle((80, 50, 130, 100), fill=0)
+    #draw.rectangle((80, 50, 130, 100), fill=0)
 
     # TODO: place text dynamically
     draw.text((2, 0), 'Cool Bird Wearing Glasses', font=font24, fill=0)
     draw.text((2, 0), 'Lichtenstein', font=font24, fill=0)
-    epd.display(imageBase)
+    epd.display(epd.getbuffer(imageBase))
+    ImageShow.show(imageBase)
     time.sleep(2)
 
     logging.info("Goto Sleep...")
