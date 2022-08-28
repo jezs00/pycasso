5597480e36b4c009feb58842855229362afce064
Jez <contact@jessfarber.com>
2022-08-28 22:08:12 +1000

    Added Providers amounts

--- constants.py
+++ constants.py
@@ -4,11 +4,15 @@ from enum import Enum
 
 
 class Providers(Enum):
+    HISTORIC = 0
     STABLE = 1
     DALLE = 2
+    HISTORIC_AMOUNT = 1
+    STABLE_AMOUNT = 0
+    DALLE_AMOUNT = 0
 
 
-class StabilityVariables(Enum):
+class Stability(Enum):
     KEY = "STABILITY_KEY"
     HOST = "STABILITY_HOST"
     DEFAULT_HOST = "grpc.stability.ai:443"
