b3fa5716ce578ee1dcfd20785a310bb814e19db1
Jez <contact@jessfarber.com>
2022-08-28 21:33:14 +1000

    Added stability constants

--- constants.py
+++ constants.py
@@ -6,3 +6,9 @@ from enum import Enum
 class Providers(Enum):
     STABLE = 1
     DALLE = 2
+
+
+class StabilityVariables(Enum):
+    ENVIRONMENT = "STABILITY_KEY"
+    HOST = "STABILITY_HOST"
+    DEFAULT_HOST = "grpc.stability.ai:443"
