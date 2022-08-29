d92f7d01a331975fb5c65739c6e7a6721d8ab4f9
Jez <contact@jessfarber.com>
2022-08-29 18:52:44 +1000

    Cleaned up file, constants for keychain and keyname. Possible future APIs there too.

--- constants.py
+++ constants.py
@@ -7,9 +7,17 @@ class Providers(Enum):
     HISTORIC = 0
     STABLE = 1
     DALLE = 2
+    MIDJOURNEY = 3
+
     HISTORIC_AMOUNT = 1
     STABLE_AMOUNT = 0
     DALLE_AMOUNT = 0
+    MIDJOURNEY_AMOUNT = 0
+
+    KEYCHAIN = "PYCASSO"
+    STABLE_KEYNAME = "STABILITY"
+    DALLE_KEYNAME = "DALLE"
+    MIDJOURNEY_KEYNAME = "MIDJOURNEY"
 
 
 class Stability(Enum):
