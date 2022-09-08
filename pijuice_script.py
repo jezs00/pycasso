ba0815bcdcb445d1a1600f9357515943f5f9d896
jezs00 <contact@jessfarber.com>
2022-09-08 10:45:20 +0100

    Updated to allow running from service/cron/etc

---
+++ pijuice_script.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+from pijuice import PiJuice
+import logging
+import os
+import sys
+
+try:
+	pijuice = PiJuice(1, 0x14)
+except:
+	print("Cannot create pijuice object")
+	sys.exit()
+
+# TODO: set constants for pijuice statuses
+
+power_status = pijuice.status.GetStatus()['data']['powerInput']
+
+logging.info(f"Power status is \'{power_status}\'")
+
+if power_status != 'NOT_PRESENT':
+	# If power not plugged in, run pycasso and shut down
+	os.system(f"sudo dbus-run-session -- bash {os.path.dirname(os.path.abspath(__file__))}/run.sh")
+	sys.exit()
+	# Remove power to PiJuice MCU IO pins
+	# pijuice.power.SetSystemPowerSwitch(0)
+
+	# In 10 seconds we are not so nice - Remove 5V power to RPi
+	# pijuice.power.SetPowerOff(10)
+
+	# But try to shut down nicely first
+	# os.system("sudo shutdown -h 0")
+
--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #! usr/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo -n 'db' | gnome-keyring-daemon --unlock
-python3 pycasso.py
+python3 $SCRIPT_DIR/pycasso.py
