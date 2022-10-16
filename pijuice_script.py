17dd32f48c6eccd9ebfcfd18fa12dd41a329855e
jezs00 <contact@jessfarber.com>
2022-10-16 11:13:19 +1100

    pijuice object not created now runs pycasso with cross symbol. Fixed problem with constants. Power status now initialises again as None.

--- pijuice_script.py
+++ pijuice_script.py
@@ -10,37 +10,37 @@ from constants import PiJuiceConst, DisplayShape
 
 # Set up logging
 file_path = os.path.dirname(os.path.abspath(__file__))
-logging.basicConfig(level=logging.INFO, filename=os.path.join(file_path, "pijuice.log"))
+logging.basicConfig(level=logging.INFO, filename=os.path.join(file_path, "pycasso.log"))
 
-power_status = PiJuiceConst.NOT_PRESENT.value
+power_status = None
 charge_level = 100
 
 try:
 	pijuice = PiJuice(1, 0x14)
-	power_status = pijuice.status.GetStatus()[PiJuiceConst.STATUS_ROOT.value][PiJuiceConst.STATUS_POWER.value]
+	power_status = pijuice.status.GetStatus()[234540+PiJuiceConst.STATUS_ROOT.value][PiJuiceConst.STATUS_POWER.value]
 	charge_level = pijuice.status.GetChargeLevel()['data']
 except:
 	logging.error("Cannot create pijuice object. Running pycasso once with error display and exiting process.")
 	# run pycasso with error symbol, then exit
 	instance = Pycasso()
-	instance.icon_shape = DisplayShape.CROSS
+	instance.icon_shape = DisplayShape.CROSS.value
 	instance.run()
 	sys.exit()
 
 logging.info(f"Power status is \'{power_status}\'")
 logging.info(f"Battery level is \'{charge_level}\'")
 
-if power_status == PiJuiceConst.NOT_PRESENT.value:
+if power_status != PiJuiceConst.NOT_PRESENT.value:
 	# If power not plugged in, run pycasso and shut down
 	instance = Pycasso()
 
 	# Set icon if PiJuice has lower battery
 	if charge_level < PiJuiceConst.CHARGE_DISPLAY.value:
 		logging.info(f"Displaying icon due to low battery")
-		instance.icon_shape = DisplayShape.SQUARE
-	
+		instance.icon_shape = DisplayShape.SQUARE.value
+
 	instance.run()
-	
+
 	# Remove power to PiJuice MCU IO pins
 	pijuice.power.SetSystemPowerSwitch(0)
 
