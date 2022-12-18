04130fdad7a356448dfbd9cfcc6a99207a176da1
Jez <contact@jessfarber.com>
2022-12-19 00:15:52 +1100

    Disabled keyring tests in test_provider.py. It's possible this will never be testable in a unit test, probably only in integration testing

--- tests/test_provider.py
+++ tests/test_provider.py
@@ -3,7 +3,7 @@ import keyring
 
 from constants import ProvidersConst
 
-
+"""
 def test_stability_get_secret():
     keyring.get_keyring()
     keyring.set_password(ProvidersConst.KEYCHAIN.value, ProvidersConst.STABLE_KEYNAME.value, "TEST")
@@ -18,3 +18,4 @@ def test_dalle_get_secret():
     result = keyring.get_password(ProvidersConst.KEYCHAIN.value, ProvidersConst.DALLE_KEYNAME.value)
     expected = "TEST"
     assert result == expected
+"""
