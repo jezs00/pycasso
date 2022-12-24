006856840dc8e32bf534471f7b817294ace4f319
Jez <contact@jessfarber.com>
2022-12-24 16:47:57 +1100

    Added script to easily set keys

---
+++ set_keys.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+# Small script to set keys
+import logging
+
+from provider import StabilityProvider, DalleProvider, ProvidersConst
+
+logging.basicConfig(level=logging.DEBUG)
+
+print("Set key for provider")
+mode = int(input(f"---Available Providers---\n"
+                 f"[{ProvidersConst.STABLE.value}] - Stable Diffusion\n"
+                 f"[{ProvidersConst.DALLE.value}] - Dalle\n"
+                 f"-------------------------\n"
+                 f"Choose Provider:"))
+key = input("\nAPI key:")
+
+if mode == ProvidersConst.STABLE.value:
+    StabilityProvider.add_secret(key)
+elif mode == ProvidersConst.DALLE.value:
+    DalleProvider.add_secret(key)
+
+print("Added key OK")
