66e4e4edf375e332523bb6b3bbeec15973b8de87
Jez <contact@jessfarber.com>
2023-01-09 13:38:32 +1100

    Fixed bash location. Added quotes to directory.

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
-#! usr/bin/bash
+#! /usr/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo -n 'db' | gnome-keyring-daemon --unlock
-python3 $SCRIPT_DIR/run.py
+python3 "${SCRIPT_DIR}/run.py"
