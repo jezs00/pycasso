2ca5ed8f0f0dc7e057b77a979d0e3e94bd5e55ab
Jez <contact@jessfarber.com>
2023-01-10 14:08:34 +1100

    Fixed path insert, again.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,12 +1,6 @@
 import os
 import sys
-import piblo.constants
 
-# Remove config file if it exists here
-config_path = piblo.constants.ConfigConst.CONFIG_PATH.value
-
-# Import path for github tests
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "src")))
-
-if os.path.exists(config_path):
-    os.remove(config_path)
+# Import path for GitHub tests
+path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src"))
+sys.path.insert(0, path)
