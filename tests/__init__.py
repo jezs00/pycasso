848c714256a69e3fdcca9a5f0687b0b917970c86
Jez <contact@jessfarber.com>
2023-01-10 13:57:30 +1100

    Added path import for github unit testing

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,8 +1,12 @@
 import os
+import sys
 import piblo.constants
 
 # Remove config file if it exists here
 config_path = piblo.constants.ConfigConst.CONFIG_PATH.value
+
+# Import path for github tests
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', "src")))
 
 if os.path.exists(config_path):
     os.remove(config_path)
