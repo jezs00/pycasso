925767a2acad3d9c2e829012e9044c7ca1f6c0d6
Jez <contact@jessfarber.com>
2023-01-11 16:09:53 +1100

    Added some logging for pijuice_disable_leds.py

--- scripts/pijuice_disable_leds.py
+++ scripts/pijuice_disable_leds.py
@@ -3,5 +3,7 @@
 # Python script to disable LEDs on pijuice to avoid draining battery unnecessarily
 
 from piblo.pijuice_handler import PiJuiceHandler
+import logging
 
 PiJuiceHandler.pijuice_led_disable()
+logging.info("LEDs disabled on pijuice unit")
